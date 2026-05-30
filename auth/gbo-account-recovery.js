6d97a27c980f1bfe89e61166ee2e59b0254cec17
Thiago Neri <87212192+thiagonerieu@users.noreply.github.com>
2026-05-30 06:03:25 +0200

    3 paginas de acesso auth front-end

--- auth/gbo-account-recovery.js
+++ auth/gbo-account-recovery.js
@@ -1,15 +1,15 @@
 // JS Formulaire
-const loginForm = document.querySelector('.form');
-loginForm.addEventListener('submit',(event)=>{
+const AccountRecoveryForm = document.querySelector('.form');
+AccountRecoveryForm.addEventListener('submit',(event)=>{
     event.preventDefault();
-    console.log('Login : Formulaire soumis.');
+    console.log('Account Recovery : Formulaire soumis.');
     // Mostra uma orientação JS indicando que o formulário foi enviado, sem recarregar a página, usando event.preventDefault() para evitar o comportamento padrão de envio do formulário.
 });
 
 const inputEmail = document.querySelector('#login-email');
 const emailRegexp = /^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,6}$/;
 inputEmail.addEventListener('keyup',()=>{
-    console.log("Login :C'est ce qui est indiqué dans le champ « e-mail ».");
+    console.log("Account Recovery :C'est ce qui est indiqué dans le champ « e-mail ».");
     // Mostra uma orientação JS sobre o campo de e-mail, se o formato do e-mail é válido ou não, mudando a cor de fundo do campo para o vermelho da paleta quando não válido e azul da paleta quando válido.
     if(!emailRegexp.test(inputEmail.value)){
         inputEmail.style.backgroundColor = "#ff7b72";
@@ -24,7 +24,7 @@ inputEmail.addEventListener('keyup',()=>{
 const showPassword = document.querySelector(".form-icon-eye");
 showPassword.addEventListener('click',()=>{
     let inputPassword = document.querySelector("#login-password");
-    console.log("Login : Lorsque vous cliquez dessus, le mot de passe s'affiche ou se masque.");
+    console.log("Account Recovery : Lorsque vous cliquez dessus, le mot de passe s'affiche ou se masque.");
     // Mostra uma orientação JS icon-eye visible e hidden, indicando que o ícone de olho é clicável para mostrar ou ocultar a senha, alternando o tipo do campo de senha entre "password" e "text" e mudando a imagem do ícone de olho para refletir o estado atual (visível ou oculto).
     if (inputPassword.type === "password") {
         inputPassword.type = "text";
--- auth/gbo-signup.js
+++ auth/gbo-signup.js
@@ -1,30 +1,28 @@
 // JS Formulaire
-const loginForm = document.querySelector('.form');
-loginForm.addEventListener('submit',(event)=>{
+const signupForm = document.querySelector('.form');
+signupForm.addEventListener('submit',(event)=>{
     event.preventDefault();
-    console.log('Login : Formulaire soumis.');
+    console.log('Signup : Formulaire soumis.');
     // Mostra uma orientação JS indicando que o formulário foi enviado, sem recarregar a página, usando event.preventDefault() para evitar o comportamento padrão de envio do formulário.
 });
 
-const inputEmail = document.querySelector('#login-email');
+const inputEmail = document.querySelector('#signup-email');
 const emailRegexp = /^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,6}$/;
 inputEmail.addEventListener('keyup',()=>{
-    console.log("Login :C'est ce qui est indiqué dans le champ « e-mail ».");
+    console.log("Signup :  C'est ce qui est indiqué dans le champ « e-mail ».");
     // Mostra uma orientação JS sobre o campo de e-mail, se o formato do e-mail é válido ou não, mudando a cor de fundo do campo para o vermelho da paleta quando não válido e azul da paleta quando válido.
     if(!emailRegexp.test(inputEmail.value)){
         inputEmail.style.backgroundColor = "#ff7b72";
-        inputEmail.style.color = "#1E293B";
-        }
+    }
     else{
         inputEmail.style.backgroundColor = "#818CF8";
-        inputEmail.style.color = "#E2E8F0";
     }            
 });
 
-const showPassword = document.querySelector(".form-icon-eye");
+const showPassword = document.querySelector(".form-icon.form-icon-eye");
 showPassword.addEventListener('click',()=>{
-    let inputPassword = document.querySelector("#login-password");
-    console.log("Login : Lorsque vous cliquez dessus, le mot de passe s'affiche ou se masque.");
+    let inputPassword = document.querySelector("#signup-password");
+    console.log("Signup : Lorsque vous cliquez dessus, le mot de passe s'affiche ou se masque.");
     // Mostra uma orientação JS icon-eye visible e hidden, indicando que o ícone de olho é clicável para mostrar ou ocultar a senha, alternando o tipo do campo de senha entre "password" e "text" e mudando a imagem do ícone de olho para refletir o estado atual (visível ou oculto).
     if (inputPassword.type === "password") {
         inputPassword.type = "text";
